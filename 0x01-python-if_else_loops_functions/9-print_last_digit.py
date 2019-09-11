5fb50606080781a857c135e1d3bd1beb888510f7
juanquinterodev <896@holbertonschool.com>
2019-09-11 00:07:39 -0500

    fix last dig

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        dig = (number * -1) % 10
-    else:
+    if number >= 0:
         dig = number % 10
+    else:
+        dig = (number * -1) % 10
     print("{}".format(dig), end="")
     return dig
