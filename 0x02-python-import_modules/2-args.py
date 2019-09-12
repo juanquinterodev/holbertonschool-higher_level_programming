ee2f8f48cf2bf29e9affa08cd1af932a19cee729
juanquinterodev <896@holbertonshool.com>
2019-09-12 23:18:00 +0000

    fix pep8

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,5 +7,5 @@ elif argc == 2:
     print("{} argument:".format(argc - 1))
 else:
     print("{} arguments:".format(argc - 1))
-for i in range(1,argc,1):
+for i in range(1, argc, 1):
     print("{}: {}".format(i, sys.argv[i]))
