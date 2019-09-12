f793fbd97878301aa2bd05afe80c33dc5f9b9c6a
juanquinterodev <896@holbertonshool.com>
2019-09-12 23:15:49 +0000

    add arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+argc = len(sys.argv)
+if argc == 1:
+    print("{} arguments.".format(argc - 1))
+elif argc == 2:
+    print("{} argument:".format(argc - 1))
+else:
+    print("{} arguments:".format(argc - 1))
+for i in range(1,argc,1):
+    print("{}: {}".format(i, sys.argv[i]))
