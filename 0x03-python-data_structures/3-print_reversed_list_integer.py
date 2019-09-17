7c215bf4b73139bf018eaa7cc09412b63b175bfb
juanquinterodev <896@holbertonshool.com>
2019-09-17 22:07:23 +0000

    fix indent

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,4 +4,5 @@ def print_reversed_list_integer(my_list=[]):
     for i in range(len(my_list)):
         if(my_list) is None:
             return(None)
-        print("{:d}".format(my_list[i]))
+        else:
+            print("{:d}".format(my_list[i]))
