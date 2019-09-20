178168edf6f64c8c43f777ed8f7b9668ca604821
juanquinterodev <896@holbertonschool.com>
2019-09-19 23:48:13 -0500

    fix condition

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    return calc(map(lambda x: x*number, my_list)) if my_list is not None
+    return calc(map(lambda x: x*number, my_list)) if my_list is not None else None
