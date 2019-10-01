23f03684f8eac73d7e4214d85265b460ca33700f
juanquinterodev <896@holbertonschool.com>
2019-09-30 23:37:35 -0500

    add division list

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    mylist = []
+    for i in range(list_length):
+        try:
+            res = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print('wrong type')
+            res = 0
+        except IndexError:
+            print('out of range')
+            res = 0
+        except ZeroDivisionError:
+            print('division by 0')
+        finally:
+            mylist.append(res)
+        return mylist
