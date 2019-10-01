c64f22c89c1832f0fdda2c9278dc276259b99eea
juanquinterodev <896@holbertonschool.com>
2019-09-30 23:18:44 -0500

    fix return

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,8 +2,8 @@
 def safe_print_division(a, b):
     try:
         res = a / b
-    except ZeroDivisionError:
-        return = None
+    except:
+        return None
     finally:
         print("Inside result: {}".format(res))
     return res
