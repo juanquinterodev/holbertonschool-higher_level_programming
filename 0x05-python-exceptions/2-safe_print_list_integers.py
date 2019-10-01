687333e657ad411162893eae125b63812f03be64
juanquinterodev <896@holbertonschool.com>
2019-09-30 22:52:05 -0500

    fix sintax

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,10 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     c = 0
     for i in range(x):
         try:
-            print("{:d}.format(my_list[i]), end='')
+            print("{:d}".format(my_list[i]), end='')
             c += 1
-        except
+        except:
             pass
     print()
     return c
-    
