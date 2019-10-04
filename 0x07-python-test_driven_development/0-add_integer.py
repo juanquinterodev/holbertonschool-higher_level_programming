50d3aba0c3a7f3cc35dc1e8f4626f16a29383840
juanquinterodev <896@holbertonshool.com>
2019-10-04 20:19:34 +0000

    add integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if type(a) is int or type(a) is float
+	pass
+    else
+	raise TypeError("a must be an integer")
+    if type(b) is int or type(b) is float
+	pass
+    else
+	raise TypeError("b must be an integer")
+    
+    res = int(a) + int(b)
+    return res
