a718fc5623516b4e3370152737afde52bb91ff91
juanquinterodev <896@holbertonschool.com>
2019-10-05 00:33:08 -0500

    fix abs add task

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,9 +10,7 @@ def add_integer(a, b=98):
     """
     if type(a) != int and type(a) != float:
         raise TypeError('a must be an integer')
-    elif type(b) != int and type(b) != float:
+    if type(b) != int and type(b) != float:
         raise TypeError('b must be an integer')
     else:
-        if abs(a + b) == float('inf'):
-            raise OverflowError('overflow error')
-        return (int(a) + int(b))
+        return int(a) + int(b)
