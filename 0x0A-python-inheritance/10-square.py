6968b02e57bc3fc13ff5fbb394b06889feb99eaa
juanquinterodev <896@holbertonschool.com>
2019-10-15 23:13:58 -0500

    add tasks 5-11 inheritance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+
+    def __init__(self, size):
+        self.__size = size
+        Rectangle.integer_validator(self, 'size', size)
+        Rectangle.__init__(self, self.__size, self.__size)
---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+
+    def __init__(self, size):
+        self.__size = size
+        Rectangle.integer_validator(self, 'size', size)
+        Rectangle.__init__(self, self.__size, self.__size)
+    def __str__(self):
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
