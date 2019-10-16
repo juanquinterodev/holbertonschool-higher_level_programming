4b5ecfbca2b52edd1f05983a7e46e83d0512d455
juanquinterodev <896@holbertonschool.com>
2019-10-15 22:03:02 -0500

    add task 0-4 inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+function to return available methods and attr
+"""
+
+
+def lookup(obj):
+    """
+    dir to get the methods and attributes available
+    
+
+    Args:
+        obj (obj): to pass to dir
+
+    returns:
+        list: list of methods and attributes of an object
+    """
+
+    return dir(obj)
