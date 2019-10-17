2b7204be78b959b769a9c8bf9b4096a5fad63b12
juanquinterodev <896@holbertonschool.com>
2019-10-16 23:44:08 -0500

    fix 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -9,14 +9,14 @@ from sys import argv
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-name = 'add_item.json'
+filename = 'add_item.json'
 
 try:
-    new = load_from_json_file(name)
+    new = load_from_json_file(filename)
 except:
     new = []
 
 for arg in argv[1:]:
     new.append(arg)
 
-save_to_json_file(new, name)
+save_to_json_file(new, filename)
