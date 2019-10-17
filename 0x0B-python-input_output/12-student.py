7f2edc73f8ccbc9ae572e1844abc86d7a2b49b00
juanquinterodev <896@holbertonschool.com>
2019-10-16 23:58:49 -0500

    add student class

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Define a student
+"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes Student class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+
+        s = {}
+
+        for key in attrs:
+            if key in self.__dict__:
+                s[key] = self.__dict__[key]
+
+        return s
