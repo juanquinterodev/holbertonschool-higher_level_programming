00ebab689c78c06785f2e6bf314c432634d1ff42
juanquinterodev <896@holbertonschool.com>
2020-01-14 02:48:53 +0000

    fix space

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""sends a POST request to the passed URL with the email as a parameter, 
+"""sends a POST request to the passed URL with the email as a parameter,
 and displays the body of the response"""
 from urllib import request, parse
 from sys import argv
