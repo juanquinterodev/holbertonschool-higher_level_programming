40f782a29329fe1e73e32aedb360e4485c86df79
juanquinterodev <896@holbertonschool.com>
2020-02-11 14:26:38 +0000

    fix style factor indexr

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,5 +2,5 @@
 // Imports an array and computes a new array.
 const init = require('./100-data').list;
 console.log(init);
-const new_list = init.map(function (x, y) { return (x * y); });
-console.log(new_list);
+const newlist = init.map(function (x, y) { return (x * y); });
+console.log(newlist);
