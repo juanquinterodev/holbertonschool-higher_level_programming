2db78f8afff5855a29c4d2ad85c770a9491fab35
juanquinterodev <896@holbertonschool.com>
2020-02-11 06:13:23 +0000

    logme

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// prints the number of arguments already printed and the new argument value
+let sum = 0;
+exports.logMe = function (item) {
+  console.log(sum + ': ' + item);
+  sum++;
+};
